51ddd4c31bd6ba73e09fe209848b201dbee89445
Lirous587 <2115883273@qq.com>
2025-10-28 23:11:57 +0800

    feat:获取当前租户信息优化

--- src/layouts/components/admin/header/HeaderTenant.vue
+++ src/layouts/components/admin/header/HeaderTenant.vue
@@ -1,41 +1,49 @@
 <script setup lang="ts">
-import { type Tenant, type TenantPlan } from '@/api/tenant'
-import { getTenant, getTenatPlan } from '@/composable/useTenantHandler'
-import { ref } from 'vue'
+import { type Tenant } from '@/api/tenant'
+import { getTenant } from '@/composable/useTenantHandler'
+import { ref, watch } from 'vue'
 import HeaderSwitchTenant from './HeaderSwitchTenant.vue'
 import { Skeleton } from 'li-daisy'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
 
 const nowTenant = ref<Tenant>()
-getTenant().then(res => {
-  nowTenant.value = res
-})
+const getTenantHandler = (id: number) => {
+  nowTenant.value = null
+  getTenant(id).then(res => {
+    nowTenant.value = res
+  })
+}
 
-const nowTenantPlan = ref<TenantPlan>()
-getTenatPlan().then(res => {
-  nowTenantPlan.value = res
-})
+watch(
+  () => (route.params as { id?: number }).id,
+  (newId, oldId) => {
+    if (newId !== oldId) {
+      getTenantHandler(newId)
+    }
+  },
+  {
+    immediate: true,
+  }
+)
 </script>
 
 <template>
   <div class="flex items-center gap-x-1.5">
     <Skeleton :loading="!nowTenant" :delay="2000">
       <template #skeleton>
-        <div class="skeleton p-1 rounded-md w-12 h-5"></div>
+        <div class="flex gap-x-1.5">
+          <div class="skeleton p-1 rounded-md w-12 h-5"></div>
+          <div class="skeleton p-1 rounded-md w-12 h-5"></div>
+        </div>
       </template>
       <template #content>
         <span class="text-sm font-bold p-1">
-          {{ nowTenant?.name }}
+          {{ nowTenant.name }}
         </span>
-      </template>
-    </Skeleton>
-
-    <Skeleton :loading="!nowTenantPlan" :delay="2000">
-      <template #skeleton>
-        <div class="skeleton p-1 rounded-md w-12 h-5"></div>
-      </template>
-      <template #content>
         <span class="badge badge-primary badge-soft badge-sm">
-          {{ nowTenantPlan?.plan_type }}
+          {{ nowTenant.plan_type }}
         </span>
       </template>
     </Skeleton>
